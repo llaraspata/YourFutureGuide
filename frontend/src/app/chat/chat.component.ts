1cab8756ef3f98764b015f3abef9507b2207d62b
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2024-09-21 01:26:50 +0200

    WIP chat UI

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChatComponent {
-
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatComponent } from "../chat/chat.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ChatComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
