683b7712685147ccc50baa842e6a9bbf436702e8
Lucrezia LARASPATA <llaraspata@talentia-software.com>
2024-09-24 17:31:29 +0200

    further fixes

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,17 +1,18 @@
-import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Message } from './Message';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LlmMessage } from './LlmMessage';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [MatCardModule, HttpClientModule],
+  imports: [CommonModule, MatCardModule, HttpClientModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 
 
@@ -27,6 +28,16 @@ export class ChatComponent {
   careerPredictionApi = "http://127.0.0.1:8000/predict/career"
 
   isWriting = false;
+
+  apiToCall = "";
+  qst_count = 0;
+
+  finalMessage: Message = {
+    "display_order": 0,
+    "type": "R",
+    "content": "Com'è andata?",
+    "options": []
+  };
 
 
   constructor(private http: HttpClient) {
@@ -54,20 +65,40 @@ export class ChatComponent {
       }
     ];
 
+    let finalOptions = [
+      {
+        "id": 0,
+        "icon": "✅",
+        "content": "L'ho trovato interessante e piuttosto accurato!",
+        "message": ""
+      },
+      {
+        "id": 1,
+        "icon": "❌",
+        "content": "Non mi ha convinto del tutto...",
+        "message": ""
+      }
+    ];
+
+    this.finalMessage.options = finalOptions;
+
     this.order = this.chatMessagesUI.length;
   }
 
+  trackByMessageId(index: number, message: any): string {
+    return message.id;
+  }
 
   async startConversation(choosenOption: number) {
     this.isWriting = true;
 
-    var apiToCall = "";
+    this.apiToCall = "";
 
     if (choosenOption === 0) {
-      apiToCall = this.degreePredictionApi;
+      this.apiToCall = this.degreePredictionApi;
     } 
     else if (choosenOption === 1) {
-      apiToCall = this.careerPredictionApi;
+      this.apiToCall = this.careerPredictionApi;
     }
 
     this.addChoiseToChat(choosenOption);
@@ -79,22 +110,19 @@ export class ChatComponent {
       "options": []
     };
 
-    this.postStartConversation(apiToCall).subscribe((response: any) => {
+    this.postStartConversation().subscribe((response: any) => {
       this.chatMessagesLLM = response.chat_messages;
 
-      console.log(response.llm_output);
-      
       newMsg.content = response.llm_output;
-      console.log(newMsg.content);
 
       this.chatMessagesUI.push(newMsg);
+  
+      this.qst_count++;
       this.isWriting = false;
-
     });
   }
 
   addChoiseToChat(choosenOption: number) {
-
     let newMsg = {
       "display_order": this.order++,
       "type": "S",
@@ -102,20 +130,18 @@ export class ChatComponent {
       "options": []
     };
 
-    console.log(choosenOption);
-
-
     this.chatMessagesUI.push(newMsg);
   }
 
-  postStartConversation(api: string) {
+  postStartConversation() {
     const jsonPayload = {
       "usr_answer": "",
       "chat": [],
-      "qst_count": 0
+      "qst_count": 1
     };
-
-    return this.http.post(api, jsonPayload);
+    console.log(jsonPayload);
+    
+    return this.http.post(this.apiToCall, jsonPayload);
   }
 
 
@@ -133,8 +159,40 @@ export class ChatComponent {
 
     this.chatMessagesUI.push(newMsg);
     this.message.nativeElement.value = "";
+
+    this.isWriting = true;
+
+    this.postAnswer(newMsg.content).subscribe((response: any) => {
+      this.chatMessagesLLM = response.chat_messages;
+
+      let llmMsg = {
+        "display_order": this.order++,
+        "type": "R",
+        "content": response.llm_output,
+        "options": []
+      };
+
+      this.chatMessagesUI.push(llmMsg);
+      this.isWriting = false;
+      this.qst_count++;
+
+      console.log(response.end_of_chat);
+
+      if (response.end_of_chat) {
+        this.chatMessagesUI.push(this.finalMessage);
+      }
+    });
   }
 
+  postAnswer(usr_answer: string) {
+    const jsonPayload = {
+      "usr_answer": usr_answer,
+      "chat": this.chatMessagesLLM,
+      "qst_count": this.qst_count
+    };
+
+    return this.http.post(this.apiToCall, jsonPayload);
+  }
   
 
 }
